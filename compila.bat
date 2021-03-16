0ec7d3222a525416a78d962d1656b49f9ff8ce56
mtps37 <49248439+mtps37@users.noreply.github.com>
2021-03-16 19:39:18 +0100

    Añadidas versiones divIDE e IDE8

--- compila.bat
+++ compila.bat
@@ -1,1 +1,13 @@
-\php compila.php >nul
+SjAsmPlus plus3es40.asm
+SjAsmPlus plus3en40.asm
+SjAsmPlus plus3en41.asm
+SjAsmPlus plus3es41.asm
+SjAsmPlus plus3en40divmmc.asm
+SjAsmPlus plus3es40divmmc.asm
+SjAsmPlus plus3en40mmc.asm
+SjAsmPlus plus3es40mmc.asm
+SjAsmPlus plus3en40divide.asm
+SjAsmPlus plus3es40divide.asm
+SjAsmPlus plus3en40ide8.asm
+SjAsmPlus plus3es40ide8.asm
+

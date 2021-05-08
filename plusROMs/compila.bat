848f2b2b71b8020de5cd834145b06fe023e49413
antoniovillena <_@antoniovillena.es>
2021-05-08 21:57:42 +0200

    Añado taps para octocom

--- plusROMs/compila.bat
+++ plusROMs/compila.bat
@@ -4,6 +4,9 @@ SjAsmPlus plus3en41.asm
 SjAsmPlus plus3es41.asm
 SjAsmPlus plus3en40divmmc.asm
 SjAsmPlus plus3es40divmmc.asm
+GenRom t "+3e ZXMMCmod English" plus3en40divmmc.rom plus3en40divmmc.tap
+GenRom t "+3e ZXMMCmod Spanish" plus3es40divmmc.rom plus3es40divmmc.tap
+
 SjAsmPlus plus3en40mmc.asm
 SjAsmPlus plus3es40mmc.asm
 
@@ -14,4 +17,3 @@ SjAsmPlus plus3en40divide.asm
 SjAsmPlus plus3es40divide.asm
 SjAsmPlus plus3en40ide8.asm
 SjAsmPlus plus3es40ide8.asm
-
